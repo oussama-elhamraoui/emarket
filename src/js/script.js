a419ba7bbe404d8ce6987389ba1103a5d42958bf
oussama-elhamraoui <63466745+oussama-elhamraoui@users.noreply.github.com>
2020-05-11 01:37:10 +0000

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
 
     $('[data-add-to-cart]').click(function(e) {
@@ -6,3 +6,9 @@ $(document).ready(function () {
         e.stopPropagation();
     });
 });
+$('.product-option input[type="radio"]').change(size);
+
+function size() {
+    $(this).parents('.product-option').siblings().removeClass('active');
+    $(this).parents('.product-option').addClass('active');
+}
